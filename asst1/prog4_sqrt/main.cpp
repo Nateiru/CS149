7b77d758b920ceb9505da6d9ddd47f7237e2f33b
Bamboo1 <zyzhu2001@gmail.com>
2023-09-24 22:01:48 +0800

    asst1: finish proj4 and implement sqrtSimd256 using AVX1 intrinsics
    Signed-off-by: Bamboo1 <zyzhu2001@gmail.com>

--- asst1/prog4_sqrt/main.cpp
+++ asst1/prog4_sqrt/main.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <pthread.h>
 #include <math.h>
+#include <malloc.h>
 
 #include "CycleTimer.h"
 #include "sqrt_ispc.h"
@@ -9,6 +10,7 @@
 using namespace ispc;
 
 extern void sqrtSerial(int N, float startGuess, float* values, float* output);
+extern void sqrtSimd256(int N, float startGuess, float* values, float* output);
 
 static void verifyResult(int N, float* result, float* gold) {
     for (int i=0; i<N; i++) {
@@ -22,10 +24,12 @@ int main() {
 
     const unsigned int N = 20 * 1000 * 1000;
     const float initialGuess = 1.0f;
-
-    float* values = new float[N];
-    float* output = new float[N];
+    // float* values = new float[N];
+    // float* output = new float[N];
     float* gold = new float[N];
+    float* values = static_cast<float*>(aligned_alloc(N * sizeof(float), 32));
+    float* output = static_cast<float*>(aligned_alloc(N * sizeof(float), 32));
+    // float* gold = static_cast<float*>(aligned_alloc(N * sizeof(float), 32));
 
     for (unsigned int i=0; i<N; i++)
     {
@@ -34,7 +38,15 @@ int main() {
         // to you generate best and worse-case speedups
         
         // starter code populates array with random input values
-        values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
+
+        /** random */
+        values[i] = .001f + 2.998f * static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+
+        /** best case */
+        // values[i] = 2.998f;
+
+        /** worst case for SIMD */
+        // values[i] = (i % 8 == 0) ? 2.998f : 1.f;
     }
 
     // generate a gold version to check results
@@ -54,6 +66,22 @@ int main() {
     }
 
     printf("[sqrt serial]:\t\t[%.3f] ms\n", minSerial * 1000);
+
+    verifyResult(N, output, gold);
+
+    //
+    // And run the serial implementation 3 times, again reporting the
+    // minimum time.
+    //
+    double minSimd256 = 1e30;
+    for (int i = 0; i < 1; ++i) {
+        double startTime = CycleTimer::currentSeconds();
+        sqrtSimd256(N, initialGuess, values, output);
+        double endTime = CycleTimer::currentSeconds();
+        minSimd256 = std::min(minSimd256, endTime - startTime);
+    }
+
+    printf("[sqrt simd256]:\t\t[%.3f] ms\n", minSimd256 * 1000);
 
     verifyResult(N, output, gold);
 
@@ -92,12 +120,16 @@ int main() {
 
     verifyResult(N, output, gold);
 
+    printf("\t\t\t\t(%.2fx speedup from SIMD256)\n", minSerial/minSimd256);
     printf("\t\t\t\t(%.2fx speedup from ISPC)\n", minSerial/minISPC);
     printf("\t\t\t\t(%.2fx speedup from task ISPC)\n", minSerial/minTaskISPC);
 
-    delete [] values;
-    delete [] output;
+    // delete [] values;
+    // delete [] output;
     delete [] gold;
+    free(values);
+    free(output);
+    // free(gold);
 
     return 0;
 }
--- asst1/prog4_sqrt/sqrtSerial.cpp
+++ asst1/prog4_sqrt/sqrtSerial.cpp
@@ -1,7 +1,13 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 
+#ifdef __AVX__
+  #include <immintrin.h>
+#else
+  #warning No AVX support - will not compile
+#endif
 
 void sqrtSerial(int N,
                 float initialGuess,
@@ -24,5 +30,51 @@ void sqrtSerial(int N,
         }
 
         output[i] = x * guess;
+    }
+}
+
+void sqrtSimd256(int N,
+                float initialGuess,
+                float values[],
+                float output[])
+{
+    static const float kThreshold = 0.00001f;
+    __mmask8 mask;
+    __m256 guess, x, tmp, left, right;
+    __m256 const_one = _mm256_set1_ps(1.0f);
+    __m256 const_two = _mm256_set1_ps(2.0f);
+    __m256 const_three = _mm256_set1_ps(3.0f);
+    __m256 threshold = _mm256_set1_ps(kThreshold);
+    __m256 ABS = _mm256_set1_ps(-0.0f);
+
+    for (int i = 0; i < N; i += 8) {
+        x = _mm256_load_ps(values + i);                           // float x = values[i];
+        guess = _mm256_set1_ps(initialGuess);                     // float guess = initialGuess;
+        mask = 0xFF;
+        tmp = _mm256_mask_mul_ps(tmp, mask, guess, guess);        // 
+        tmp = _mm256_mask_mul_ps(tmp, mask, tmp, x);              //
+        tmp = _mm256_mask_sub_ps(tmp, mask, tmp, const_one);      //
+        tmp = _mm256_mask_andnot_ps(tmp, mask, ABS, tmp);         // float error = fabs(guess * guess * x - 1.f);
+        // 比较的操作条件 greater than 30: OP := _CMP_GT_OQ
+        mask = mask & (_mm256_cmp_ps_mask(tmp, threshold, (unsigned char)30));
+
+        while (mask) {
+            left = _mm256_mask_mul_ps(guess, mask, const_three, guess); // left = 3.f * guess
+            right = _mm256_mask_mul_ps(guess, mask, guess, guess);      //
+            right = _mm256_mask_mul_ps(right, mask, right, guess);      // 
+            right = _mm256_mask_mul_ps(right, mask, right, x);          // right = x * guess * guess * guess
+
+            left = _mm256_mask_sub_ps(left, mask, left, right);         //
+            guess = _mm256_mask_div_ps(left, mask, left, const_two);    // guess = (3.f * guess - x * guess * guess * guess) * 0.5f;
+
+            tmp = _mm256_mask_mul_ps(guess, mask, guess, guess);        // 
+            tmp = _mm256_mask_mul_ps(tmp, mask, tmp, x);                //
+            tmp = _mm256_mask_sub_ps(tmp, mask, tmp, const_one);        // float error = guess * guess * x - 1.f
+            tmp = _mm256_mask_andnot_ps(tmp, mask, ABS, tmp);           // error = fabs(error);
+            mask = mask & (_mm256_cmp_ps_mask(tmp, threshold, (unsigned char)30));
+        }
+
+        guess = _mm256_mask_mul_ps(guess, 0xFF, x, guess);
+        _mm256_store_ps(output + i, guess);
     }
 }
