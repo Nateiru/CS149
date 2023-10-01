65d1c084d8a7578234b8b78485be6677ec8424f2
Bamboo1 <zyzhu2001@gmail.com>
2023-10-01 17:18:06 +0800

    asst2(part_b): pass async test
    Signed-off-by: Bamboo1 <zyzhu2001@gmail.com>

--- asst2/part_b/tasksys.cpp
+++ asst2/part_b/tasksys.cpp
@@ -1,4 +1,6 @@
 #include "tasksys.h"
+#include "itasksys.h"
+#include <iostream>
 
 
 IRunnable::~IRunnable() {}
@@ -129,59 +131,149 @@ const char *TaskSystemParallelThreadPoolSleeping::name() {
 }
 
 TaskSystemParallelThreadPoolSleeping::TaskSystemParallelThreadPoolSleeping(
-  int num_threads): ITaskSystem(num_threads) {
-  //
-  // TODO: CS149 student implementations may decide to perform setup
-  // operations (such as thread pool construction) here.
-  // Implementations are free to add new class member variables
-  // (requiring changes to tasksys.h).
-  //
+  int num_threads): ITaskSystem(num_threads) , num_threads_(num_threads) {
+  threads_.resize(num_threads_);
+  for(int i = 0; i < num_threads_; ++i) {
+    threads_[i] = std::thread(&TaskSystemParallelThreadPoolSleeping::threadLoop, this);
+  }
+}
+
+void TaskSystemParallelThreadPoolSleeping::threadLoop() {
+  for (;;) {
+    // 等待 ready 对了有可以执行的任务
+    std::unique_lock<std::mutex> lk(mtx_);
+    not_empty_.wait(lk, [this]{
+      return !ready_.empty() || terminate_;
+    });
+    if (terminate_) {
+      break;
+    }
+    Task* task = ready_.front();
+    int stage = -1, finished = -1;
+    {
+      // 当前任务执行的阶段时 stage，进入下一阶段
+      // 如果不需要执行下一阶段从 ready_ 中删除
+      // 此时任务并不一定完成，不能 notify done
+      std::unique_lock<std::mutex> tlk(task->tmtx_);
+      stage = task->stage_;
+      if (++task->stage_ == task->total_tasks_) {
+        ready_.pop();
+      }
+    }
+    lk.unlock();
+
+    // 运行 task 任务的 stage 阶段
+    task->runnable_->runTask(stage, task->total_tasks_);
+
+    // 需要 finish 的原因是可能有多个线程通知执行 task 的不同阶段
+    // 但是都还没有完成任务
+    {
+      std::unique_lock<std::mutex> tlk(task->tmtx_);
+      finished = ++task->finished_;
+    }
+
+    // 任务完成后有两件事
+    // 1. 尝试将依赖任务从 block 移动到 ready
+    // 2. 尝试唤醒 sync
+    if (finished == task->total_tasks_) {
+      lk.lock();
+      finished_[task->id_] =  task;
+
+      // 没有任务依赖当前完成的任务
+      if(!gdep_.count(task->id_)) {
+        // 每当任务完成时，才可能唤醒 done
+        if (block_.empty() && ready_.empty()) {
+          done_.notify_one();
+        }
+        continue;
+      }
+
+      for(auto t: gdep_[task->id_]) {
+        std::unique_lock<std::mutex> tlk(t->tmtx_);
+        if (--t->dep_cnt_ == 0) {
+          block_.erase(t);
+          ready_.push(t);
+          not_empty_.notify_all();
+        }
+      }
+      // 每当任务完成时，才可能唤醒 done
+      if (block_.empty() && ready_.empty()) {
+        done_.notify_one();
+      }
+    }
+  }
 }
 
 TaskSystemParallelThreadPoolSleeping::~TaskSystemParallelThreadPoolSleeping() {
-  //
-  // TODO: CS149 student implementations may decide to perform cleanup
-  // operations (such as thread pool shutdown construction) here.
-  // Implementations are free to add new class member variables
-  // (requiring changes to tasksys.h).
-  //
+
+  {
+    std::unique_lock<std::mutex> lk(mtx_);
+    terminate_ = true;
+    not_empty_.notify_all();
+  }
+
+  for(int i = 0; i < num_threads_; ++i) {
+    threads_[i].join();
+  }
+
+  // we need delete all task pointer that construct by `new`
+  for (auto task : finished_) {
+    delete task.second;
+  }
+
+  while (!ready_.empty()) {
+    delete ready_.front();
+    ready_.pop();
+  }
+
+  while (!block_.empty()) {
+    delete *block_.begin();
+    block_.erase(block_.begin());
+  }
 }
 
 void TaskSystemParallelThreadPoolSleeping::run(IRunnable *runnable, int num_total_tasks) {
-
-
-  //
-  // TODO: CS149 students will modify the implementation of this
-  // method in Parts A and B.  The implementation provided below runs all
-  // tasks sequentially on the calling thread.
-  //
-
-  for (int i = 0; i < num_total_tasks; i++) {
-    runnable->runTask(i, num_total_tasks);
-  }
+  runAsyncWithDeps(runnable, num_total_tasks, {});
+  sync();
 }
 
 TaskID TaskSystemParallelThreadPoolSleeping::runAsyncWithDeps(IRunnable *runnable,
     int num_total_tasks,
     const std::vector<TaskID> &deps) {
 
-
-  //
-  // TODO: CS149 students will implement this method in Part B.
-  //
-
-  for (int i = 0; i < num_total_tasks; i++) {
-    runnable->runTask(i, num_total_tasks);
+  // 当前任务依赖的任务数量，类比拓扑排序 
+  // 如果依赖的任务已经完成，忽略这个依赖
+  std::unique_lock<std::mutex> lk(mtx_);
+  size_t dep_cnt = 0;
+  for (auto dep : deps) {
+    if (!finished_.count(dep)) {
+      dep_cnt++;
+    }
   }
 
-  return 0;
+  // TODO: replace new with std::shared_ptr 
+  Task* task = new Task(id_, runnable, num_total_tasks, dep_cnt);
+
+  if (!task->dep_cnt_) {
+    ready_.push(task);
+    not_empty_.notify_all();
+  } else {
+    for (auto dep : deps) {
+      if (finished_.count(dep)) continue;
+      if(gdep_.count(dep)) {
+        gdep_[dep].insert(task);
+      } else {
+        gdep_[dep] = std::unordered_set<Task*>{task};
+      }
+    }
+    block_.insert(task);
+  }
+  return id_++;
 }
 
 void TaskSystemParallelThreadPoolSleeping::sync() {
-
-  //
-  // TODO: CS149 students will modify the implementation of this method in Part B.
-  //
-
-  return;
+  std::unique_lock<std::mutex> lk(mtx_);
+  done_.wait(lk, [this]{
+    return block_.empty() && ready_.empty();
+  });
 }
--- asst2/part_b/tasksys.h
+++ asst2/part_b/tasksys.h
@@ -1,8 +1,15 @@
 #ifndef _TASKSYS_H
 #define _TASKSYS_H
 
-#include "itasksys.h"
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <cstddef>
+#include <unordered_set>
+#include <unordered_map>
+#include <condition_variable>
 
+#include "itasksys.h"
 /*
  * TaskSystemSerial: This class is the student's implementation of a
  * serial task execution engine.  See definition of ITaskSystem in
@@ -53,6 +60,18 @@ public:
   void sync();
 };
 
+struct Task {
+  TaskID id_{-1};                 // 任务编号
+  IRunnable *runnable_{nullptr};  // 执行任务
+  int stage_{0};                  // 该任务有很多子任务，当前执行第 stage_ 子任务
+  int finished_{0};               // 子任务完成的数量
+  int total_tasks_{0};            // 子任务总数量
+  size_t dep_cnt_{0};             // 依赖任务数量，当 dep_cnt_ = 0 时加入 ready
+  std::mutex tmtx_;               // task 互斥锁
+  Task(TaskID id, IRunnable *runnable, int total_tasks, size_t dep_cnt):
+    id_(id), runnable_(runnable), total_tasks_(total_tasks), dep_cnt_(dep_cnt) {}
+};
+
 /*
  * TaskSystemParallelThreadPoolSleeping: This class is the student's
  * optimized implementation of a parallel task execution engine that uses
@@ -68,6 +87,23 @@ public:
   TaskID runAsyncWithDeps(IRunnable *runnable, int num_total_tasks,
                           const std::vector<TaskID> &deps);
   void sync();
+
+private:
+  int num_threads_{0};
+  std::vector<std::thread> threads_;
+  std::queue<Task *> ready_;            // ready_ 中的任务此时依赖的任务已经全部完成
+  std::unordered_set<Task *> block_;    // block_ 依赖任务没被完成暂时被阻塞
+  std::condition_variable not_empty_;   // 通知 ready_ 已经有任务能够执行
+  std::mutex mtx_;                      // 一把大锁保平安
+  std::condition_variable done_;        // 同步任务全部完成
+  TaskID id_{0};                        // 全局的任务 id 分配，从 0 开始
+  bool terminate_{false};               // 是否终止
+
+  // 依赖关系图：哪些任务依赖于 task_id 
+  std::unordered_map<TaskID, std::unordered_set<Task *>> gdep_;
+  // 已经完成的任务，有可能任务 b 虽然依赖 a，但是 a 已经完成，此时需要注意任务 dep_cnt 的维护
+  std::unordered_map<TaskID, Task*> finished_;
+  void threadLoop();
 };
 
 #endif
