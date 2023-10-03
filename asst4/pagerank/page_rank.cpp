6f3fdba42e2e1066a48c83ca5eab886bc85f080b
Bamboo1 <zyzhu2001@gmail.com>
2023-10-04 01:27:14 +0800

    asst4(part1): add serial pagerank
    Signed-off-by: Bamboo1 <zyzhu2001@gmail.com>

--- asst4/pagerank/page_rank.cpp
+++ asst4/pagerank/page_rank.cpp
@@ -28,7 +28,45 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
   for (int i = 0; i < numNodes; ++i) {
     solution[i] = equal_prob;
   }
-  
+
+  double* tempArray = new double[g->num_nodes];
+
+  bool converged = false;
+  while (!converged) {
+    for (Vertex v = 0; v < g->num_nodes; ++v) {
+      tempArray[v] = 0.0;
+    }
+    for (Vertex v = 0; v < g->num_nodes; ++v) {
+      for (const Vertex* iv = incoming_begin(g, v); iv != incoming_end(g, v); ++iv) {
+        tempArray[v] += solution[*iv] / static_cast<double>(outgoing_size(g, *iv));
+      }
+    }
+    for (Vertex v = 0; v < g->num_nodes; ++v) {
+      tempArray[v] = tempArray[v] * damping + (1.0 - damping) / static_cast<double>(numNodes);
+    }
+
+    double t = 0.0;
+    for (Vertex v = 0; v < g->num_nodes; ++v) {
+      if(outgoing_size(g, v) == 0) {
+          t += damping * solution[v] / static_cast<double>(numNodes);
+      }
+    }
+
+    for (Vertex v = 0; v < g->num_nodes; ++v) {
+      tempArray[v] += t;
+    }
+
+    double globalDiff = 0.0;
+    for (Vertex v = 0; v < g->num_nodes; ++v) {
+      globalDiff += abs(tempArray[v] - solution[v]);
+    }
+    for (Vertex v = 0; v < g->num_nodes; ++v) {
+      solution[v] = tempArray[v];
+    }
+    converged = globalDiff < convergence;
+  }
+
+  delete[] tempArray;
   
   /*
      CS149 students: Implement the page rank algorithm here.  You
